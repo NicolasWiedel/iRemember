631c2feb642f3f165b7b2bead10ca3a3cf4e4e98
Nicolas Wiedel <nwiedel@alte-hof-apotheke.com>
2020-12-07 23:48:31 +0100

    Rechtschreibfehler korrigiert

--- iRemember/src/de/javadevblog/iremember/controller/BaseController.java
+++ iRemember/src/de/javadevblog/iremember/controller/BaseController.java
@@ -7,7 +7,7 @@ public abstract class BaseController {
 	protected ViewFactory viewFactory;
 	private String fxmlName;
 	
-	public BaseController(ViewFactory viewfactory, String fxmlName) {
+	public BaseController(ViewFactory viewFactory, String fxmlName) {
 		this.viewFactory = viewFactory;
 		this.fxmlName = fxmlName;
 	}
