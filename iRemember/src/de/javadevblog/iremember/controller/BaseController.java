10925c448452664d083ba17cc98380975e3710da
Nicolas Wiedel <nwiedel@alte-hof-apotheke.com>
2020-12-08 23:44:41 +0100

    Grundlage steht!

--- iRemember/src/de/javadevblog/iremember/controller/BaseController.java
+++ iRemember/src/de/javadevblog/iremember/controller/BaseController.java
@@ -1,15 +1,37 @@
 package de.javadevblog.iremember.controller;
 
 import de.javadevblog.iremember.view.ViewFactory;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
-public abstract class BaseController {
+public class BaseController {
 
 	protected ViewFactory viewFactory;
 	private String fxmlName;
-	
+
+	@FXML
+	private Button btnAdresses;
+
+	@FXML
+	private Button btnMails;
+
+	@FXML
+	private Button btnDates;
+
+	@FXML
+	private Button btnNotes;
+
 	public BaseController(ViewFactory viewFactory, String fxmlName) {
 		this.viewFactory = viewFactory;
 		this.fxmlName = fxmlName;
+	}
+	
+	@FXML
+	public void changeCatgorie(ActionEvent e) {
+		if ( e.getSource() == btnNotes) {
+			System.out.println("Notizen gedrückt!");
+		}
 	}
 
 	public ViewFactory getViewFactory() {
@@ -27,6 +49,5 @@ public abstract class BaseController {
 	public void setFxmlName(String fxmlName) {
 		this.fxmlName = fxmlName;
 	}
-	
-	
+
 }
--- iRemember/src/de/javadevblog/iremember/view/ViewFactory.java
+++ iRemember/src/de/javadevblog/iremember/view/ViewFactory.java
@@ -5,18 +5,21 @@ import de.javadevblog.iremember.controller.NoteViewController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class ViewFactory {
 
 	private Stage stage;
 	private Scene scene;
+	private BaseController baseController;
 	private BaseController controller;
 	private double sceneWidth;
 	private double sceneHeight;
 	
 	public ViewFactory() {
 		stage = new Stage();
+		baseController = new BaseController(this, "baseview.fxml");
 		sceneWidth = 900;
 		sceneHeight = 600;
 		showNoteScene();
@@ -29,17 +32,22 @@ public class ViewFactory {
 	}
 
 	private void initializeScene(BaseController controller) {
+		FXMLLoader baseLoader = new FXMLLoader(getClass().getResource(baseController.getFxmlName()));
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+		baseLoader.setController(baseController);
 		loader.setController(controller);
+		BorderPane mainPane;
 		Parent parent;
 		try {
+			mainPane = baseLoader.load();
 			parent = loader.load();
 		} catch (Exception e) {
 			e.printStackTrace();
 			return;
 		}
+		mainPane.setCenter(parent);
 		setSceneSice();
-		scene = new Scene(parent, sceneWidth, sceneHeight);
+		scene = new Scene(mainPane, sceneWidth, sceneHeight);
 		
 		stage.setScene(scene);
 	}
